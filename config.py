1a0d961c31735eb9360ec63853bb4b38d5b27b0f
Victor Popov <119051119+Vantany@users.noreply.github.com>
2023-11-19 13:46:26 +0300

    new commit

---
+++ config.py
@@ -0,0 +1,10 @@
+mail_pass = "cb6WMQvuyKAgLpd38hwt"
+username = "6113-10.05.03@mail.ru"
+imap_server = "imap.mail.ru"
+
+bot_key = "6561097655:AAEzdEPQpMNWF8m7VVrOGbPuMEpz7vCy6dw"
+chat_id = "-1001902231920"
+
+send_attach = True
+
+encoding="utf-8"
